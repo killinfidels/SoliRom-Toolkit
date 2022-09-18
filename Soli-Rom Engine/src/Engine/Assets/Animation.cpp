181164d350dd3290ca7abc2bdf9daf3447ff41b8
killinfidels <mahdi0800@gmail.com>
2022-09-18 16:22:20 +0200

    Made animations, only missing load from .txt... i think

---
+++ Soli-Rom Engine/src/Engine/Assets/Animation.cpp
@@ -0,0 +1,172 @@
+#include "precompiledheaders.h"
+#include "Engine/Windows/Window.h"
+#include "Engine/Assets/Texture.h"
+#include "Engine/Tools/Timer.h"
+//#include "Engine/Assets/AssetManager.h"
+#include "Engine/Log.h"
+#include "Animation.h"
+
+namespace SoliRom::Asset
+{
+	Animation::Animation(std::string _name, std::string _path, int _frameTime)
+	{
+		path = _path;
+		name = _name;
+		defaultFrameTime = _frameTime;
+
+		Init();
+	}
+
+	Animation::Animation(std::string _name, std::string _path)
+	{
+		path = _path;
+		name = _name;
+
+		Init();
+	}
+
+	void Animation::Init()
+	{
+		//check if file exists
+		if (SDL_RWFromFile(path.c_str(), "r") == NULL)
+		{
+			SR_CORE_WARN("Animation failed to load: %s. Error: %s", path.c_str(), SDL_GetError());
+		}
+		else
+		{
+			//load if text file
+			if (path.find(".txt") != std::string::npos)
+			{
+				//load text file
+			}
+
+			//auto find frames from path to first if not text file
+			//seperate frame number from path
+			size_t pos = path.rfind("_");
+
+			int frameNumber;
+			//catches the error from stoi for not having a number in the string
+			try
+			{
+				frameNumber = std::stoi(path.substr(pos + 1, path.rfind(".") - pos - 1));
+			}
+			catch (...)
+			{
+				frameNumber = 0;
+			}
+
+			if (pos == std::string::npos || frameNumber != 1)
+			{
+				SR_CORE_WARN("Animation path does not lead to first frame: '%s'.\nAnimations should be format like this: 'Animation_1.png', etc.", path.c_str());
+			}
+			else
+			{
+				std::string firstHalf = path.substr(0, pos + 1);
+				std::string lastHalf = path.substr(path.rfind("."));
+
+				name = firstHalf.substr(firstHalf.rfind("/") + 1, firstHalf.rfind("_") - firstHalf.rfind("/") - 1);
+
+				int i = 1;
+
+				//increment frame number and load as many frames as possible
+				do
+				{
+					std::string framePath = firstHalf + std::to_string(i) + lastHalf;
+					frame.resize(i);
+					frameTime.resize(i);
+					frame[i - 1] = new Texture(framePath.substr(framePath.rfind("/") + 1, framePath.rfind(".") - framePath.rfind("/") - 1), framePath, true);
+					frameTime[i - 1] = defaultFrameTime;
+					i++;
+				} while (SDL_RWFromFile((firstHalf + std::to_string(i) + lastHalf).c_str(), "r") != NULL);
+
+				SR_CORE_TRACE("Animation '%s' loaded with %i frames", name.c_str(), i - 1);
+			}
+		}
+	}
+
+	bool Animation::Load()
+	{
+		if (window == NULL)
+		{
+			SR_CORE_WARN("Animation: '%' loading failed, no target window to render.\nPath: '%s'", name.c_str(), path.c_str());
+			return loaded;
+		}
+		else
+		{
+			for (int i = 0; i < frame.size(); i++)
+			{
+				frame[i]->window = window;
+				frame[i]->Load();
+			}
+
+		loaded = true;
+		}
+
+		return loaded;
+	}
+
+	Texture* Animation::Get()
+	{
+		if (name == "NO ANIMATION")
+		{
+			SR_CORE_WARN("No animation to play!");
+
+			return NULL;
+		}
+		else
+		{
+			frameTimer.update();
+
+			finished = false;
+
+			if (frameTimer.checkElapsed(frameTime[currentFrame]))
+			{
+				currentFrame++;
+				if (currentFrame >= frame.size())
+				{
+					finished = true;
+					currentFrame = 0;
+				}
+
+				frameTimer.Reset();
+			}
+
+		}
+
+		return frame[currentFrame];
+	}
+
+	void Animation::Reset()
+	{
+		frameTimer.Reset();
+
+		currentFrame = 0;
+
+		finished = false;
+	}
+
+	void Animation::Start()
+	{
+		frameTimer.Start();
+		playing = true;
+	}
+
+	void Animation::Skip()
+	{
+		frameTimer.Reset();
+		currentFrame++;
+		if (currentFrame >= frame.size())
+			currentFrame = 0;
+	}
+
+	void Animation::Stop()
+	{
+		frameTimer.Stop();
+		playing = false;
+	}
+
+	bool Animation::Done()
+	{
+		return finished;
+	}
+}
---
+++ Soli-Rom Engine/src/Engine/Assets/Animation.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "Engine/Tools/Timer.h"
+
+namespace SoliRom
+{
+	class AssetManager;
+}
+
+namespace SoliRom::Asset
+{
+	class Animation
+	{
+	private: //fucky change back to private when fix
+		//animation file format should be "file_1.png"
+
+		//format for animations should be "name_x" where x is the frame number
+
+		//path to either first frame or txt file including all frames
+		Animation(std::string _name, std::string _path);
+		Animation(std::string _name, std::string _path, int _frameTime);
+
+		void Init();
+		bool Load();
+	public:
+		bool CreateTextFile();
+
+		//plays the animation and returns current frame;
+		Texture* Get();
+
+		void Reset();
+		void Start();
+		void Skip();
+		void Stop();
+		//returns true if animation is done?
+		bool Done();
+
+		bool switchFrame(int _frame, std::string _path);
+		bool switchFrame(int _frame, Texture* _texture);
+		bool switchFrameTime(int _frame, int _frameTime);
+
+	private:
+		//path to first frame or .txt file
+		std::string name = "NO ANIMATION";
+		std::string path = "NO PATH";
+
+		//frame array of textures
+		std::vector<Texture*> frame;
+		//milliseconds
+		std::vector<int> frameTime;
+
+		Window* window = NULL;
+
+		Timer frameTimer;
+
+		//flags
+		int currentFrame = 0;
+		int defaultFrameTime = 500;
+		bool playing = false;
+		bool finished = false;
+		bool loaded = false;
+
+	public:
+		friend class ::SoliRom::AssetManager;
+	};
+}
+
