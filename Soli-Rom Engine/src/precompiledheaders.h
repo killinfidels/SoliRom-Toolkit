b8a60c7d6482b14dc240d845d819911a572a0722
killinfidels <mahdi0800@gmail.com>
2022-10-10 23:37:53 +0200

    SDL TTF STUFF

--- Soli-Rom Engine/src/precompiledheaders.h
+++ Soli-Rom Engine/src/precompiledheaders.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #pragma warning(disable: 26819 26451 4006)
-#include "SDL.h"
-#include "SDL_image.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 #pragma warning(default: 26819 26451 4006)
 
 #include <stdio.h>
